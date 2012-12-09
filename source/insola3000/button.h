c215df33c13b88fdad43dddb823dff48b85051ca
Roberto Chauderlot Marchamalo <rchauderlot@users.noreply.github.com>
2012-12-10 00:51:00 +0100

    Buggy implementation of the long press feature on the button.

--- source/insola3000/button.h
+++ source/insola3000/button.h
@@ -9,17 +9,23 @@ class Button {
 public:
 
   Button(int pin, void(*buttonPressedCallback)(void), long debounceDelay);
+  Button(int pin, void(*buttonPressedCallback)(void), long debounceDelay, void(*buttonLongPressedCallback)(void), long longPressRepetitionDelay);
   void update();
+    
     
 private:
 
   void (*_buttonPressedCallback)(void);
+  void (*_buttonLongPressedCallback)(void);
+  long _longPressRepetitionDelay;
   int  _pin;
   long _debounceDelay;
   int  _buttonState; // Debounced state
   int  _lastButtonState; // transitory state
   long _lastDebounceTime;
-
+  long _lastLongPressTime;
+  
+  
 };
 
 #endif /* _BUTTON_ */
