a0a63e2706be3298f84985fc9449d37db1618fba
Roberto Chauderlot Marchamalo <rchauderlot@gmail.com>
2012-12-09 23:29:05 +0100

    Added config file.

---
+++ source/insola3000/config.h
@@ -0,0 +1,17 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+//#define _INSOLA_3000_DEBUG_
+#define SERIAL_BAUDRATE 9600
+
+#define UP_BUTTON_PIN         5
+#define DOWN_BUTTON_PIN       6
+#define STARTSTOP_BUTTON_PIN  7
+
+#define LCD_DATA_PIN   3
+#define LCD_CLOCK_PIN  4
+#define LCD_UPDATE_PIN 2
+
+
+#endif /* _CONFIG_H_ */
+
