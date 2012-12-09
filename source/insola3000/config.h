d0d43ae51b03e4e2f4db9bcdbeef1afab5a1948c
Roberto Chauderlot Marchamalo <rchauderlot@gmail.com>
2012-12-10 00:51:24 +0100

    More detailed configuration file

--- source/insola3000/config.h
+++ source/insola3000/config.h
@@ -1,16 +1,22 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-//#define _INSOLA_3000_DEBUG_
+#define _INSOLA_3000_DEBUG_
 #define SERIAL_BAUDRATE 9600
 
 #define UP_BUTTON_PIN         5
 #define DOWN_BUTTON_PIN       6
 #define STARTSTOP_BUTTON_PIN  7
+#define DEBOUNCE_DELAY        20
+#define LONGPRESS_DELAY       500
 
 #define LCD_DATA_PIN   3
 #define LCD_CLOCK_PIN  4
 #define LCD_UPDATE_PIN 2
+
+#define OUTPUT_PIN 8
+
+#define MAX_PROGRAMMABLE_TIME 90*60;
 
 
 #endif /* _CONFIG_H_ */
