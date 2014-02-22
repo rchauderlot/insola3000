203a357eb57a37e1cc2c6d9717483e4913498c92
Roberto Chauderlot Marchamalo <rchauderlot@users.noreply.github.com>
2014-02-22 12:54:43 +0100

    Improvements in lcd lib

--- source/insola3000/lcd.cpp
+++ source/insola3000/lcd.cpp
@@ -4,38 +4,63 @@
 
 
 // Variables will change:
+
 // LDC control
+char space = B00000000;
+char e_letter = B01111001;
+char dash = B01000000;
+char dot  = B10000000;
 //                  0         1         2         3         4         5         6         7         8         9         A         b         C         D         E         F
 char lcdDigits[] = {B00111111,B00000110,B01011011,B01001111,B01100110,B01101101,B01111101,B00000111,B01111111,B01101111,B00110111,B01111100,B00111001,B01011110,B01111001,B01110001};
 //                   A         b         C         d         E         F         G         H         I         J         K*NO_SUP  L         M*NO_SUP  n         o         p         q         r         S         t         u         v*NO_SUP  w*NO_SUP  x*NO_SUP  y         z
-char lcdLetters[] = {B01110111,B01111100,B00111001,B01011110,B01111001,B01110001,B01111101,B01110110,B00000110,B00011111,B00000000,B00111000,B00110111,B01010100,B01011100,B01110011,B01100111,B01010000,B01101101,B01111000,B00011100,B00111110,B00000000,B00000000,B01101110,B01011011};
-
+char lcdLetters[] = {B01110111,B01111100,B00111001,B01011110,e_letter,B01110001,B01111101,B01110110,B00000110,B00011111,B00000000,B00111000,B00110111,B01010100,B01011100,B01110011,B01100111,B01010000,B01101101,B01111000,B00011100,B00111110,B00000000,B00000000,B01101110,B01011011};
 
 
 Lcd::Lcd(int updateRegisterPin, int clockSerialPin, int dataSerialPin) {
   this->_updateRegisterPin = updateRegisterPin;
   this->_clockSerialPin = clockSerialPin;
   this->_dataSerialPin = dataSerialPin;
-  
-  pinMode(_updateRegisterPin, OUTPUT);
-  pinMode(_clockSerialPin   , OUTPUT);
-  pinMode(_dataSerialPin    , OUTPUT);
-
+  if (this->_updateRegisterPin >= 0) {  
+    pinMode(this->_updateRegisterPin, OUTPUT);
+  }
+  if (this->_clockSerialPin >=0 ) {
+    pinMode(this->_clockSerialPin   , OUTPUT);
+  }
+  if (this->_dataSerialPin >= 0) {
+    pinMode(this->_dataSerialPin    , OUTPUT);
+  }
 }
 
 
 void Lcd::sendBit(boolean active) {
 
-    digitalWrite(_clockSerialPin, LOW);
-    if (active) { 
-      digitalWrite(_dataSerialPin, HIGH);
-    }else {
-      digitalWrite(_dataSerialPin, LOW);
+    if (_clockSerialPin >= 0) {
+      digitalWrite(_clockSerialPin, LOW);
     }
-    digitalWrite(_clockSerialPin, HIGH);
+    if (_dataSerialPin >= 0) {
+      if (active) {
+        digitalWrite(_dataSerialPin, HIGH);
+      }else {
+        digitalWrite(_dataSerialPin, LOW);
+      }
+    }
+    if (_clockSerialPin >= 0) {
+      digitalWrite(_clockSerialPin, HIGH);
+    }
 }
 
 void Lcd::sendByte(char character) {
+    if (_updateRegisterPin >= 0) {
+    digitalWrite(_updateRegisterPin, LOW);
+  }
+  internalSendByte(character);
+  if (_updateRegisterPin >= 0) {
+    digitalWrite(_updateRegisterPin, HIGH);
+  }
+
+}
+
+void Lcd::internalSendByte(char character) {
   for (int i=7; i>=0; i--) {
     sendBit(bitRead(character, i));
   }
@@ -49,8 +74,19 @@ void Lcd::sendChar(char character) {
   Serial.println(character);
 #endif
 
+  if (_updateRegisterPin >= 0) {
+    digitalWrite(_updateRegisterPin, LOW);
+  }
+  internalSendChar(character);
+  if (_updateRegisterPin >= 0) {
+    digitalWrite(_updateRegisterPin, HIGH);
+  }
+}
+
+
+void Lcd::internalSendChar(char character) {
+
   char lcdCharacter=0x00;
-  
   if (character >= '0' && character <= '9') {
     lcdCharacter=lcdDigits[character-'0'];
   }
@@ -60,33 +96,99 @@ void Lcd::sendChar(char character) {
   if (character >= 'a' && character <= 'z') {
     lcdCharacter = lcdLetters[character-'a'];
   }
-  digitalWrite(_updateRegisterPin, LOW);
-  sendByte(lcdCharacter);
-  digitalWrite(_updateRegisterPin, HIGH);
+  if (character == ' ') {
+    lcdCharacter = space;
+  }
+  if (character == '-') {
+    lcdCharacter = dash;
+  }
+  internalSendByte(lcdCharacter);
 }
 
-void Lcd::sendDigit(int zeroToNineDigit) {
+void Lcd::sendDigit(int number, int numberOfDigitsToSend, char fillChar, boolean lsb,int delayBetweenDigits) {
 
 #ifdef _LCD_DEBUG_
   Serial.print("sendDigit ");
-  Serial.println(zeroToNineDigit);
+  Serial.println(number);
 #endif
-  digitalWrite(_updateRegisterPin, LOW);
-  sendByte(lcdDigits[zeroToNineDigit]);
-  digitalWrite(_updateRegisterPin, HIGH);
+
+  int multiplier = 1;
+  for (int i=0; i < numberOfDigitsToSend; i++) {
+    multiplier = multiplier * 10;
+  }
+
+  char buffer[numberOfDigitsToSend + 1];
+  buffer[numberOfDigitsToSend] = 0;
+  
+  if (number / multiplier == 0 && number >= 0) {
+    int shiftedNum = number;
+    for (int i=numberOfDigitsToSend - 1; i >= 0; i--) {
+      int digit = shiftedNum % 10;
+      if (shiftedNum > 0 || i == numberOfDigitsToSend - 1) {
+        buffer[i] = '0' + digit;
+      } else {
+        buffer[i] = fillChar;
+      }
+      shiftedNum = shiftedNum / 10;
+    } 
+  } else if (number * 10 / multiplier == 0 && number <= 0) {
+    int shiftedNum = number * -1;
+    boolean sentMinusSign=false;
+    for (int i=numberOfDigitsToSend - 1; i >= 0; i--) {
+      int digit = shiftedNum % 10;
+      if (shiftedNum > 0 || i == numberOfDigitsToSend - 1) {
+        buffer[i] = '0' + digit;
+      } else if (!sentMinusSign) {
+        buffer[i] = dash;
+        sentMinusSign = true;
+      } else {
+        buffer[i] = fillChar;
+      }
+      shiftedNum = shiftedNum / 10;
+    } 
+  } else { 
+    for (int i=numberOfDigitsToSend - 1; i >=  0; i--) {
+       if (i == numberOfDigitsToSend - 1) {
+         buffer[i]= 'E';
+       } else {
+           buffer[i]= ' ';
+       }
+    }
+  }
+  sendCharString(buffer, lsb, delayBetweenDigits);
 }
 
-void Lcd::sendCharString(char zeroEndedCharPointer[], int delayBetweenDigits) {
+void Lcd::sendCharString(char zeroEndedCharPointer[], boolean lsb, int delayBetweenDigits) {
 #ifdef _LCD_DEBUG_
   Serial.print("sendCharString ");
   Serial.println(zeroEndedCharPointer);
 #endif
   char * currentChar = zeroEndedCharPointer;
-  while (*currentChar) {  
+  
+  if (_updateRegisterPin >= 0) {
     digitalWrite(_updateRegisterPin, LOW);
-    sendChar(*currentChar);
+  }
+  if (lsb) {
+    while (*currentChar) {
+      currentChar++;
+    } 
+    while (currentChar != zeroEndedCharPointer) {  
+      currentChar--;
+      sendChar(*currentChar);
+      if (delayBetweenDigits > 0) {
+        delay(delayBetweenDigits);
+      }
+    }    
+  } else {
+    while (*currentChar) {  
+      sendChar(*currentChar);
+      if (delayBetweenDigits > 0) {
+        delay(delayBetweenDigits);
+      }
+      currentChar++;
+    }   
+  }
+  if (_updateRegisterPin >= 0) {
     digitalWrite(_updateRegisterPin, HIGH);
-    delay(delayBetweenDigits);
-    currentChar++;
   }
 }
